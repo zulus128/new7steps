d66421b27282e4818b2e6ce13c4840b2349b8e2f
Zul <Zulus@Zuls-Mac.local>
2012-09-25 05:44:44 -0700

    ff12345r5rq55

---
+++ new7steps/AboutController.h
@@ -0,0 +1,13 @@
+//
+//  AboutController.h
+//  new7steps
+//
+//  Created by Zul on 9/25/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutController : UIViewController
+
+@end
---
+++ new7steps/AboutController.m
@@ -0,0 +1,38 @@
+//
+//  AboutController.m
+//  new7steps
+//
+//  Created by Zul on 9/25/12.
+//
+//
+
+#import "AboutController.h"
+
+@interface AboutController ()
+
+@end
+
+@implementation AboutController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
